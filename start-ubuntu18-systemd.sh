99651dd4d2fa0a05215352561a908ba01baa6591
Lee Johnson <lee.james.johnson@gmail.com>
2023-02-28 12:09:53 -0500

    master - 	modified:   start-ubuntu18-systemd.sh

--- start-ubuntu18-systemd.sh
+++ start-ubuntu18-systemd.sh
@@ -3,7 +3,7 @@
 DOCKER_CONTAINER_DISTRO=ubuntu1804
 DOCKER_CMD=/lib/systemd/systemd
 
-EXEC_CMD="start-systemd-distro.sh ${DOCKER_CONTAINER_DISTRO}"
+EXEC_CMD="start-systemd-distro.sh ${DOCKER_CONTAINER_DISTRO} ${DOCKER_CMD}"
 echo "${EXEC_CMD}"
 
 ${EXEC_CMD}
