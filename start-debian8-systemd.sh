1e635eb90c4defd6ad6ef8540ddd9610b410690e
Lee Johnson <lee.james.johnson@gmail.com>
2023-02-28 11:13:31 -0500

    master - 	new file:   start-debian8-systemd.sh

---
+++ start-debian8-systemd.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+DOCKER_CONTAINER_DISTRO=debian8
+DOCKER_CMD=/lib/systemd/systemd
+
+EXEC_CMD="start-systemd-distro.sh ${DOCKER_CONTAINER_DISTRO}"
+echo "${EXEC_CMD}"
+
+${EXEC_CMD}
