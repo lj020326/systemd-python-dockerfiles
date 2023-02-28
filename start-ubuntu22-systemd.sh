bd97c5f1bcc1a7b90f8d88a8b5e4d0ef9e36ef2c
Lee Johnson <lee.james.johnson@gmail.com>
2023-02-28 11:07:15 -0500

    master - 	new file:   start-centos8-systemd.sh 	renamed:    start-debian-systemd.sh -> start-debian10-systemd.sh 	new file:   start-debian9-systemd.sh 	renamed:    start-ubuntu-systemd.sh -> start-ubuntu20-systemd.sh 	renamed:    start-centos-systemd.sh -> start-ubuntu22-systemd.sh

--- start-ubuntu22-systemd.sh
+++ start-ubuntu22-systemd.sh
@@ -1,17 +1,19 @@
 #!/usr/bin/env bash
 
-DOCKER_CONTAINER_NAME=systemd-centos8
+DOCKER_CONTAINER_NAME=systemd-ubuntu22
 #DOCKER_NAMESPACE=media.johnson.int:5000
 DOCKER_NAMESPACE=lj020326
-DOCKER_IMAGE=${DOCKER_NAMESPACE}/centos8-systemd-python:latest
+DOCKER_IMAGE=${DOCKER_NAMESPACE}/ubuntu2204-systemd-python:latest
+#DOCKER_CMD=/lib/systemd/systemd
 DOCKER_CMD=/usr/sbin/init
 
 ## ref: https://github.com/j8r/dockerfiles
+## ref: https://stackoverflow.com/questions/53383431/how-to-enable-systemd-on-dockerfile-with-ubuntu18-04
 docker run \
   --rm \
   --name ${DOCKER_CONTAINER_NAME} \
   --privileged \
-  -v /sys/fs/cgroup:/sys/fs/cgroup:ro \
+  -v /sys/fs/cgroup:/sys/fs/cgroup:rw \
   --tmpfs /run \
   --tmpfs /tmp \
   -d \
